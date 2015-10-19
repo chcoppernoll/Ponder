04f5ddfdd77ba78efe76907fc3b6c3ce96677b64
Chris <crcopper@mtu.edu>
2015-10-19 16:03:52 -0400

    Basic Client/Server functionality

--- Ponder/src/network/Server.java
+++ Ponder/src/network/Server.java
@@ -1,5 +1,27 @@
 package network;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class Server {
+	private static ServerSocket sock = null;
+
+	public static void main(String[] args) {
+		Server server = new Server();
+		server.run();
+	}
+
+	public void run() {
+		System.out.println("Accepting");
+		try {
+			sock = new ServerSocket(7777);
+			sock.accept();
+			System.out.println("Accepted");
+			sock.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Failed");
+			e.printStackTrace();
+		}
+	}
 }
