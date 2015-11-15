79cf5243068d4ba9f8f3530bd1aa92017352508e
hGriff0n <ghooper96@gmail.com>
2015-11-14 23:08:49 -0500

    Started NetworkPlayer implementation
    Determined the sequence of events for implementing NetworkPlayer.
    onTurnStart will be used to initialize communication and the event list.
    onTurnEnd will consist of a simple for-each loop over the event list
    turnOver will handle network communication and event loading

--- Ponder/src/game/NetworkPlayer.java
+++ Ponder/src/game/NetworkPlayer.java
@@ -1,21 +1,27 @@
 package game;
 
+import java.util.LinkedList;
+
 import gui.SwingGraphics;
 import network.Client;
 
 public class NetworkPlayer implements Player {
+	private LinkedList<Event> move;
 
 	public void onTurnStart(SwingGraphics graphics, Client net) {
 		//send a message
+		move = new LinkedList<>();
 	}
 
 	public void onTurnEnd(SwingGraphics graphics, Client net) {
-		
+		for (Event e : move) graphics.runEvent(e);
 	}
 
+	// Communicate with server/load "unseen" events into move
 	public boolean turnOver(SwingGraphics graphics, Client net) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		
+		return move.getLast() instanceof TurnEvent;
 	}
 
 }
